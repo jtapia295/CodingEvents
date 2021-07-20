687a0313640dff3b3c45503485f1145a666cc365
jtapia295 <85265752+jtapia295@users.noreply.github.com>
2021-07-20 17:17:55 -0400

    Complete CH13 Exercise

---
+++ Controllers/EventController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coding_Events.Controllers
+{
+    [Route("/Events")]
+    public class EventController : Controller
+    {
+        static private Dictionary<string, string> Events = new Dictionary<string, string> { };
+        public IActionResult Index()
+        {
+            ViewBag.Events = Events;
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Add")]
+        public IActionResult AddEvent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult AddEvent(string EventName, string EventDescription)
+        {
+            if (!String.IsNullOrEmpty(EventName))
+            {
+                Events.Add(EventName, EventDescription);
+            }
+            return Redirect("/Events");
+        }
+    }
+}
